19a929983c9664af7b22c811201b3c7932d0885f
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2020-12-07 00:57:57 -0600

    misc

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"adventofcode/day1"
+	"fmt"
 
 	"github.com/fatih/color"
 )
 
 func main() {
+	var dayAnswer int
 	color.Green("Welcome to my 2020 Advent of Code!")
+	color.Blue("Choose a day challenge: (1-25)")
 
-	day1.DisplayExpenses()
+	_, err := fmt.Scan(&dayAnswer)
+	if err != nil {
+		color.Red("could not read your input")
+		color.Yellow("run this program again...")
+		return
+	}
+
+	switch dayAnswer {
+	case 1:
+		day1.DisplayExpenses()
+		return
+	default:
+		color.Yellow(fmt.Sprintf("bad input...%v", dayAnswer))
+	}
 }
