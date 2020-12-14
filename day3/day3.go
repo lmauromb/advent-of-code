cb4e69f885a33dc74d7505edac2c01557479d0ce
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2020-12-13 22:44:30 -0600

    add day3 setup

---
+++ day3/day3.go
@@ -0,0 +1,29 @@
+package day3
+
+import (
+	"bufio"
+	"os"
+)
+
+const inputPath = "./day3raw-input.txt"
+
+func getLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, nil
+}
